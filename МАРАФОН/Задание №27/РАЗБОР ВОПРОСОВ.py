c7f45ccc8b3f96c5497ddf20d0de8e09945cb1e4
KINGFERZ <semenmosh01@gmail.com>
2026-06-12 19:01:32 +0800

    МАРАФОН ДЕНЬ 3

---
+++ МАРАФОН/Задание №27/РАЗБОР ВОПРОСОВ.py
@@ -0,0 +1,103 @@
+# ЗАДАЧА №1
+from lib2to3.pgen2.grammar import opmap_raw
+
+c1 = center(cl1)
+c2 = center(cl2)
+
+z1 = [] # 114
+z2 = [] # 95
+
+print(len(z1))
+print(len(z2))
+
+A1 = c2[0]
+A2 = c1[1]
+
+
+kr1 = [] # 15
+kr2 = [] # 9
+kr3 = [] # 13
+print(len(kr1))
+print(len(kr2))
+print(len(kr3))
+B1 = dist(c1, c2)
+B2 = max([dist(c1, p) for p in kr1] + [dist(c2, p) for p in kr2] + [dist(c3, p) for p in kr3])
+
+a = []
+for p in kr1:
+    a.append(dist(c1, p))
+for p in kr2:
+    a.append(dist(c2, p))
+for p in kr3:
+    a.append(dist(c3, p))
+
+# ЗАДАЧА №2
+
+c1 = center(cl1)
+c2 = center(cl2)
+
+
+A1 = min(dist(c1, orange_gig), dist(c2, orange_gig))
+A2 = max(dist(c1, orange_gig), dist(c2, orange_gig))
+
+print(len(cl1)) # 141
+print(len(cl2)) # 190
+print(len(cl3)) # 237
+
+z3 = []
+z2 = []
+
+if h[1] in '89'
+if h[1] in '0123'
+
+B1 = len(z3)
+B2 = len(z2)
+
+# ЗАДАЧА №3
+
+c1 = center(cl1) # 121
+c2 = center(cl2) # 135   [x, y]
+
+A1 = 0
+for p in cl2:
+    if p[1] < c2[1]:
+        A1 += 1
+print(A1)
+
+A2 = abs(c1[0] - c2[0])
+
+c1 = center(cl1) # 367
+c2 = center(cl2) # 89
+c3 = center(cl3) # 164
+
+c = 0
+for p in cl2:
+    if c2[0] - 0.9 < p[0] < c2[0] + 0.9 and c2[1] - 0.9 < p[1] < c2[1] + 0.9:
+        c += 1
+
+print(c)
+
+# ЗАДАЧА №4
+
+c1 = center(cl1)
+c2 = center(cl2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
